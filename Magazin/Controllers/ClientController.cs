d9db909674b44770279e154410e2589e64cf25ce
Arturchickbak <83332216+Arturchickbak@users.noreply.github.com>
2023-12-30 10:02:31 +0300

    Add files via upload

---
+++ Magazin/Controllers/ClientController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Svyaznoi.Context;
+using Svyaznoi.Context.Contracts.Models;
+
+namespace Svyaznoi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IContext context1;
+        public ClientController(IContext context1)
+        {
+            this.context1 = context1;
+        }
+        [HttpGet] //localhost:111224/group
+        public IActionResult GetAllPlatelshik()
+        {
+            var clientlist = context1.Clients.ToList();
+            return Ok(clientlist);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var clientlist = context1.Clients.FirstOrDefault(x => x.Id == id);
+            return Ok(clientlist);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var group1 = context1.Clients.FirstOrDefault(x => x.Id == id);
+            if (group1 != null)
+            {
+                context1.Clients.Remove(group1);
+            }
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Create(Client model)
+        {
+            var item1 = new Client
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Number = model.Number,
+                Email= model.Email,
+                Card = model.Card,
+            };
+            context1.Clients.Add(item1);
+            context1.SaveChanges();
+            return Ok(item1);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(Guid Id, Client model)
+        {
+            var group1 = context1.Clients.FirstOrDefault(x => x.Id == Id);
+            if (group1 != null)
+            {
+                return NotFound();
+            }
+            group1.Name = model.Name;
+            group1.Number = model.Number;
+            group1.Email = model.Email;
+            group1.Card = model.Card;
+            context1.SaveChanges();
+
+            return Ok(group1);
+        }
+    }
+}
---
+++ Magazin/Controllers/MagazinController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Svyaznoi.Context;
+using Svyaznoi.Context.Contracts.Models;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace Svyaznoi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MagazinController : ControllerBase
+    {
+        private readonly IContext context1;
+        public MagazinController(IContext context1)
+        {
+            this.context1 = context1;
+        }
+
+        [HttpGet] //localhost:111224/group
+        public IActionResult GetAllNakladnaya()
+        {
+            var magazinlist = context1.Magazins.ToList();
+            return Ok(magazinlist);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var magazinlist = context1.Magazins.FirstOrDefault(x => x.Id == id);
+            return Ok(magazinlist);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Context.Contracts.Models.Magazin model)
+        {
+            var item = new Context.Contracts.Models.Magazin
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Address = model.Address,
+                City = model.City,  
+            };
+            context1.Magazins.Add(item);
+            context1.SaveChanges();
+            return Ok(item);
+        }
+
+        [HttpDelete("{id}")] 
+        public IActionResult Delete(Guid id) 
+        {
+            var group = context1.Magazins.FirstOrDefault(x => x.Id == id);
+            if (group != null)
+            {
+                context1.Magazins.Remove(group);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(Guid Id, Context.Contracts.Models.Magazin model)
+        {
+         var group = context1.Magazins.FirstOrDefault(x => x.Id == Id);
+            if (group != null) 
+            {
+                return NotFound();
+            }
+            group.Name = model.Name;
+            group.Address = model.Address;
+            group.City = model.City;    
+
+            context1.SaveChanges();
+
+            return Ok(group);
+        }
+    }
+}
